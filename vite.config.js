7187cc37e50ef0674dbc8fb6429ad8bc023c314c
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-02-27 09:37:37 +0100

    viteconfig

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+// /** @type {import('vite').UserConfig} */
+
+export default {}
+// import { readFileSync, existsSync, unlinkSync, writeFileSync } from 'fs'
+// import path from 'path'
+// export default {
+//   // 設定オプション
+//   build: {
+//     minify: false,
+//     rollupOptions: {
+//       external: (id) => id.startsWith('three') || id === 'lil-gui', // `three` と `lil-gui` を除外
+//       output: {
+//         entryFileNames: 'index.js', // メインのJSファイル名を `index.js` に固定
+//         chunkFileNames: '[name].js', // チャンクのファイル名を変更しない
+//         assetFileNames: '[name][extname]',
+//       },
+//     },
+
+//     target: 'esnext',
+//     polyfillModulePreload: false,
+//   },
+//   plugins: [
+//     {
+//       name: 'inject-script',
+//       writeBundle() {
+//         const distPath = path.resolve('./', 'dist')
+//         console.log(distPath)
+//         const htmlPath = path.join(distPath, 'index.html')
+//         const jsPath = path.join(distPath, 'index.js')
+
+//         if (existsSync(htmlPath) && existsSync(jsPath)) {
+//           let html = readFileSync(htmlPath, 'utf-8')
+//           const scriptContent = readFileSync(jsPath, 'utf-8')
+
+//           // 既存の <script> タグを削除して新しいスクリプトを追加
+//           html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
+//           html = html.replace('</body>', `<script>\n${scriptContent}\n</script>\n</body>`)
+
+//           writeFileSync(htmlPath, html, 'utf-8')
+//           unlinkSync(jsPath) // index.js を削除
+
+//           console.log('✅ index.js を index.html に埋め込みました')
+//         }
+//       },
+//     },
+//   ],
+// }
