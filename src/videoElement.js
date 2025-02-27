b7f99027190cf4c301768a9160d6c88091f90232
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-02-27 10:56:26 +0100

    動画を外部から取得

--- src/videoElement.js
+++ src/videoElement.js
@@ -1,9 +1,10 @@
-import videoUrl from './images/video.mp4'
-
 export const generateVideoElement = () => {
   const video = document.createElement('video')
+  const videoUrl =
+    'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4'
   console.log(videoUrl)
   video.src = videoUrl
+  video.crossOrigin = 'anonymous'
   video.load()
   video.muted = true
   video.loop = true
